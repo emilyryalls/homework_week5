16f680b899089a7d96b0fe2c9f2a5b0548622587
Emily Ryalls <emilyryalls99@gmail.com>
2025-02-16 14:24:39 +0000

    CREATE: Exercise 14 files

---
+++ exercise14_emily.py
@@ -0,0 +1,25 @@
+# Create a game of rock paper scissors
+# The user will play against the computer
+# You should design a program that does the following:
+# Prompt the user to enter a value: R, P or S
+# The programme should convert this value into Rock, Paper or Scissors respectively
+# Asks the computer to generate a random value between 0 and 2
+# Convert the computer's choice. 0 becomes Rock; 1 becomes Paper; 2 becomes Scissors
+# Compare the user's choices with the computer's choice to display a message indicating whether the user won, lost or drew against the computer
+# Showcase what you have learned about conditional statements and create your own functions
+
+
+# Create functions to do the above, and store in a separate file called module_emily
+# Then think of loops that will decide what functions needs to be used
+# Think about how scores could be stored
+
+
+input("Input R, P or S:")
+
+# use if statements
+# if input is this, covert to this
+# then perform computer generated, and then convert it
+# print computer output
+# another if statement, print diff messages if winning or losing
+# loop for more inputs, doesn't need an end -
+# refer to pin one for to keep prompting inputs and using while loops
---
+++ module_emily.py
@@ -0,0 +1,18 @@
+# Create a game of rock paper scissors
+# The user will play against the computer
+# You should design a program that does the following:
+# Prompt the user to enter a value: R, P or S
+# The programme should convert this value into Rock, Paper or Scissors respectively
+# Asks the computer to generate a random value between 0 and 2
+# Convert the computer's choice. 0 becomes Rock; 1 becomes Paper; 2 becomes Scissors
+# Compare the user's choices with the computer's choice to display a message indicating whether the user won, lost or drew against the computer
+# Showcase what you have learned about conditional statements and create your own functions
+
+# convert function for user input
+# give options for capitalised and if a space is given afterwards
+
+
+# computer generated function
+# randint method
+
+# convert computer choice
